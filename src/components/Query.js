85f11c195fa458087227da1903836ce3e5211c5c
Parth Arora <partharora.india@gmail.com>
2021-07-24 23:52:03 +0530

    Added search in previous/saved queries

--- src/components/Query.js
+++ src/components/Query.js
@@ -1,9 +1,11 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import MainContext from "../utils/MainContext"
 
 const Query = (props) => {
 
   const { setQuery } = useContext(MainContext)
+  const [searchQuery, setSearchQuery] = useState('')
+  const [list, setList] = useState(props.queryList)
 
   const renderQueryList = (arr) => {
     return arr.map(i => {
@@ -13,10 +15,16 @@ const Query = (props) => {
         </div>)
     })
   }
+  useEffect(() => {
+    setList(props.queryList.filter(i => i.toLowerCase().includes(searchQuery.toLowerCase())))
+  }, [searchQuery])
 
   return (
     <div className='query-wrapper'>
-      {renderQueryList(props.queryList)}
+      <div className='search-bar'>
+        <input placeholder="Search.." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+      </div>
+      {list.length > 0 ? renderQueryList(list) : <div className='placeholder-text'><span className='fa fa-exclamation-circle'></span><p>No queries found.</p></div>}
     </div>
   )
 }
